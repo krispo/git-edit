41fd49c44cc648e039a923bef3be4be977252e0b
krispo <konstantin.skipor@gmail.com>
2014-07-14 16:39:38 +0600

    updated with readme

--- Readme.md
+++ Readme.md
@@ -2,3 +2,50 @@ Git-Edit
 ==
 
 A small javascript plugin that allows to edit web pages directly from the browser at specific place with live preview and commit the changes to github.
+
+It uses internally:
+
+* [Ace Editor](https://github.com/ajaxorg/ace) - awesome code editor.
+* [Github](https://github.com/michael/github) - a higher-level wrapper around the Github API.
+* [Font Awesome](https://github.com/FortAwesome/Font-Awesome) - The iconic font and CSS toolkit.
+* [Hint](https://github.com/chinchang/hint.css) - A tooltip library in CSS.
+
+## How to use
+
+Add this scripts to the end of your html `body`:
+```html
+<link rel="stylesheet" href="https://rawgit.com/krispo/git-edit/master/git-edit.css">
+<script src="https://rawgit.com/krispo/git-connect/master/git-connect.js"></script>
+<script src="https://rawgit.com/krispo/git-edit/master/git-edit.js"></script>
+```
+And initialize [connection](https://github.com/krispo/git-connect) with this script:
+```html
+<script>
+    window.connection({
+        client_id: "sdfjhg23jhg345j33io", //your application client_id
+        proxy: "http://git-proxy.herokuapp.com", //your git proxy
+        owner: "krispo", //your github username
+        reponame: "git-edit" //your reponame
+    })
+</script>
+```
+
+You should mark the html element, that you want to edit, with `gitedit` class,
+and add info with `data-*` attribute for branch and main file, for example:
+```html
+<body class="gitedit" data-branch="gh-pages" data-file="index.html">
+    ...
+</body>
+```
+
+Now if you deploy your app, you can call editor by `CTRL + Click` with click on your marked element.
+And then `edit` web page and `commit` the changes. You web page will updated immediately.
+
+Actually, you web page can be edited by any github user that connected to github through your app.
+And these changes will be like a `pull requests`.
+So, after `commit & pull` this repo will be forked automatically to this user github account.
+
+---
+Try [demo](http://krispo.github.io/git-edit).
+
+
--- git-edit.js
+++ git-edit.js
@@ -8,7 +8,8 @@ var ge = {
     github: null,       // github api
     baserepo: null,     // base repository from we read content
     headrepo: null,     // head repository for not owner users that want to pull request to base repo.
-    username: null      // github username
+    username: 'guest',  // github username, default - guest
+    userinfo: null      // user info
 };
 
 // include external scripts
@@ -33,6 +34,16 @@ document.addEventListener('IsConnectedToGithubEvent', function(e){
 
     //get base github repo using connection config
     ge.baserepo = ge.github.getRepo(ge.connection.config['owner'], ge.connection.config['reponame']);
+
+    //get username and userinfo
+    ge.connection.withCredentials(function(err, username, access_token, userinfo){
+        if (err === null){
+            ge.username = username;
+            ge.userinfo = userinfo;
+        } else {
+            console.log('Credentials are not valid... \nError: '+err);
+        }
+    })
 });
 
 // IsDisconnectedFromGithubEvent event is emitted by 'git-connect.js' plugin
@@ -41,6 +52,7 @@ document.addEventListener('IsDisconnectedFromGithubEvent', function(e){
     ge.connection = null;
     ge.github = null;
     ge.baserepo = null;
+    ge.username = 'guest';
 });
 
 // Initialize editor on click for any element that marked with class 'gitedit'
@@ -58,74 +70,68 @@ function addGitEditListeners(){
 // Initialize git editor
 function initEditor(parent, event){
     if (ge.github){
-        ge.connection.withCredentials(function(err, username, access_token, userinfo){
-            if (err === null){
-                ge.parentNode = parent;
-                ge.username = username;
-                // create editor element
-                ge.element = document.createElement('x');
-                ge.element.className += ' fullScreen';
-                ge.element.setAttribute('role','splitH');
-                ge.element.style.top = Math.round(event.y + window.pageYOffset) + 'px';
-                ge.element.style.left = Math.round(event.x + window.pageXOffset) + 'px';
-                ge.parentNode.appendChild(ge.element);
-                // get content from base repository
-                readFromRepo();
-                ge.element.innerHTML =
-                        '<nav class="e-header">' +
-                            '<button onclick="toggleCssClass(ge.element, \'fullScreen\'); toggleCssClass(this.querySelector(\'i\'),\'fa-expand\'); toggleCssClass(this.querySelector(\'i\'),\'fa-compress\')"><i class="fa fa-expand"></i></button>' +
-                            '<div class="labels">' +
-                                '<span>' + ge.username + '</span>' +
-                                '<label><input type="radio" name="viewType" checked="checked" value="splitH"/>SplitH</label>' +
-                                '<label><input type="radio" name="viewType" value="splitV"/>SplitV</label>' +
-                                '<label><input type="radio" name="viewType" value="code"/>Code</label>' +
-                                '<label><input type="radio" name="viewType" value="preview"/>Preview</label>' +
-                            '</div>' +
-                        '</nav>' +
-                        '<div class="e-body">' +
-                            '<div role="input"><pre class=e id=editor></pre></div>' +
-                            '<div role="output"><iframe class=e id=preview></iframe></div>' +
-                        '</div>' +
-                        '<div class="e-footer">' +
-                            '<img src='+userinfo['avatar_url']+'>' +
-                            '<div class="commit-form">' +
-                                '<h3 class="heading">Commit changes</h3>' +
-                                '<input id=message class="message" placeholder="Update '+ge.parentNode.dataset['file']+'">' +
-                                '<textarea id=description class="description" placeholder="Add an optional extended description..."></textarea>' +
-                                '<div class="actions"><button class="cancel-btn" onclick="removeEditor()">Cancel</button><button class="commit-btn"  onclick="writeToRepo(ge.editor.getSession().getValue(), message.value, description.value)">Commit & Pull</button></div>' +
-                            '</div>' +
-                        '</div>';
+        ge.parentNode = parent;
+        // create editor element
+        ge.element = document.createElement('x');
+        ge.element.className += ' fullScreen';
+        ge.element.setAttribute('role','splitH');
+        ge.element.style.top = Math.round(event.y + window.pageYOffset) + 'px';
+        ge.element.style.left = Math.round(event.x + window.pageXOffset) + 'px';
+        ge.parentNode.appendChild(ge.element);
+        ge.element.innerHTML =
+            '<nav class="e-header">' +
+                '<button onclick="toggleCssClass(ge.element, \'fullScreen\'); toggleCssClass(this.querySelector(\'i\'),\'fa-expand\'); toggleCssClass(this.querySelector(\'i\'),\'fa-compress\')"><i class="fa fa-expand"></i></button>' +
+                '<div class="labels">' +
+                    '<span>' + ge.username + '</span>' +
+                    '<label><input type="radio" name="viewType" checked="checked" value="splitH"/>SplitH</label>' +
+                    '<label><input type="radio" name="viewType" value="splitV"/>SplitV</label>' +
+                    '<label><input type="radio" name="viewType" value="code"/>Code</label>' +
+                    '<label><input type="radio" name="viewType" value="preview"/>Preview</label>' +
+                '</div>' +
+            '</nav>' +
+            '<div class="e-body">' +
+                '<div role="input"><pre class=e id=editor></pre></div>' +
+                '<div role="output"><iframe class=e id=preview></iframe></div>' +
+            '</div>' +
+            '<div class="e-footer">' +
+                '<img src='+ge.userinfo['avatar_url']+'>' +
+                '<div class="commit-form">' +
+                    '<h3 class="heading">Commit changes</h3>' +
+                    '<input id=message class="message" placeholder="Update '+ge.parentNode.dataset['file']+'">' +
+                    '<textarea id=description class="description" placeholder="Add an optional extended description..."></textarea>' +
+                    '<div class="actions"><button class="cancel-btn" onclick="removeEditor()">Cancel</button><button class="commit-btn"  onclick="writeToRepo(ge.editor.getSession().getValue(), message.value, description.value)">Commit & Pull</button></div>' +
+                '</div>' +
+            '</div>';
 
-                // initialize ace editor
-                ge.editor = ace.edit(editor);
-                ge.editor.setTheme("ace/theme/twilight");
-                ge.editor.setOptions({
-                    enableBasicAutocompletion: true,
-                    enableSnippets: true,
-                    enableLiveAutocompletion: false
-                });
+        // get content from base repository
+        readFromRepo();
 
-                ge.editor.getSession().setMode("ace/mode/html");
-                ge.editor.getSession().on('change', function(){
-                    preview.frameElement.srcdoc=ge.editor.getSession().getValue();
-                });
+        // initialize ace editor
+        ge.editor = ace.edit(editor);
+        ge.editor.setTheme("ace/theme/twilight");
+        ge.editor.setOptions({
+            enableBasicAutocompletion: true,
+            enableSnippets: true,
+            enableLiveAutocompletion: false
+        });
 
-                // Set tooltips
-                if (ge.username !== ge.connection.config['owner']){
-                    var commitBtn = ge.element.querySelector(".commit-btn");
-                    commitBtn.className += ' hint--top hint--no-animate';
-                    commitBtn.setAttribute('data-hint','The repo will automatically forked.');
-                }
+        ge.editor.getSession().setMode("ace/mode/html");
+        ge.editor.getSession().on('change', function(){
+            preview.frameElement.srcdoc=ge.editor.getSession().getValue();
+        });
 
-                // view switcher
-                var nav = ge.element.querySelector(".e-header");
-                nav.addEventListener("click", function(){
-                    ge.element.setAttribute("role", nav.querySelector("input:checked").value);
-                    ge.editor.resize();
-                })
-            } else {
-                alert('Credentials are not valid... \nError: '+err);
-            }
+        // Set tooltips
+        if (ge.username !== ge.connection.config['owner']){
+            var commitBtn = ge.element.querySelector(".commit-btn");
+            commitBtn.className += ' hint--top hint--no-animate';
+            commitBtn.setAttribute('data-hint','The repo will automatically forked.');
+        }
+
+        // view switcher
+        var nav = ge.element.querySelector(".e-header");
+        nav.addEventListener("click", function(){
+            ge.element.setAttribute("role", nav.querySelector("input:checked").value);
+            ge.editor.resize();
         })
     } else {
         alert('You should connect to Github.');
@@ -160,41 +166,45 @@ function readFromRepo(){
 // Write the changes to github.
 // If user is not owner then baserepo is automatically forked to headrepo and any changes are pulled from head to base.
 function writeToRepo(content, msg, desc){
-    // if user is owner
-    if (ge.username === ge.connection.config['owner']){
-        ge.baserepo.write(ge.parentNode.dataset['branch'], ge.parentNode.dataset['file'], content, msg, function(err, sha) {
-            if (err === null) {
-                alert('The changes are commited successfully!');
-                removeEditor();
-            } else {
-                alert('Content is not updated... \nError: '+err);
-            }
-        });
+    if (ge.github){
+        // if user is owner
+        if (ge.username === ge.connection.config['owner']){
+            ge.baserepo.write(ge.parentNode.dataset['branch'], ge.parentNode.dataset['file'], content, msg, function(err, sha) {
+                if (err === null) {
+                    alert('The changes are commited successfully!');
+                    removeEditor();
+                } else {
+                    alert('Content is not updated... \nError: '+err);
+                }
+            });
+        } else {
+            // for not owner users the base repo should be forked automatically
+            ge.baserepo.fork(function(err, fork) {
+                if (err === null) {
+                    ge.headrepo = ge.github.getRepo(ge.username, fork.name);
+                    ge.headrepo.write(ge.parentNode.dataset['branch'], ge.parentNode.dataset['file'], content, msg, function(err, sha) {
+                        if (err === null) {
+                            var pull = {
+                                title: msg,
+                                body: desc,
+                                base: ge.parentNode.dataset['branch'],
+                                head: ge.username + ":" + ge.parentNode.dataset['branch']
+                            };
+                            ge.baserepo.createPullRequest(pull, function(err, pullRequest) {
+                                alert('The changes are commited & pulled successfully!');
+                                removeEditor();
+                            });
+                        } else {
+                            alert('Content is not updated in Your forked repository... \nError: '+err);
+                        }
+                    });
+                } else {
+                    alert('Base repository is not forked... \nError: '+err);
+                }
+            });
+        }
     } else {
-        // for not owner users the base repo should be forked automatically
-        ge.baserepo.fork(function(err, fork) {
-            if (err === null) {
-                ge.headrepo = ge.github.getRepo(ge.username, fork.name);
-                ge.headrepo.write(ge.parentNode.dataset['branch'], ge.parentNode.dataset['file'], content, msg, function(err, sha) {
-                    if (err === null) {
-                        var pull = {
-                            title: msg,
-                            body: desc,
-                            base: ge.parentNode.dataset['branch'],
-                            head: ge.username + ":" + ge.parentNode.dataset['branch']
-                        };
-                        ge.baserepo.createPullRequest(pull, function(err, pullRequest) {
-                            alert('The changes are commited & pulled successfully!');
-                            removeEditor();
-                        });
-                    } else {
-                        alert('Content is not updated in Your forked repository... \nError: '+err);
-                    }
-                });
-            } else {
-                alert('Base repository is not forked... \nError: '+err);
-            }
-        });
+        alert('Sorry, you are not connected to github...')
     }
 };
 
